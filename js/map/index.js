30af4523dabfedb435e94c3761725e05c769cdca
liliyuan <782821705@qq.com>
2020-03-10 17:10:22 +0800

    基本界面完成，准备开始和后端交互，注：界面的板块的不同颜色并非最终效果，仅用于测试

--- js/map/index.js
+++ js/map/index.js
@@ -3679,8 +3679,6 @@ var data=[
 
 var myChart = echarts.init(document.getElementById('china-map'));
 
-var oBack = document.getElementById("back");
-
 var provinces = ['shanghai', 'hebei', 'shanxi', 'neimenggu', 'liaoning', 'jilin', 'heilongjiang', 'jiangsu', 'zhejiang', 'anhui', 'fujian', 'jiangxi', 'shandong', 'henan', 'hubei', 'hunan', 'guangdong', 'guangxi', 'hainan', 'sichuan', 'guizhou', 'yunnan', 'xizang', 'shanxi1', 'gansu', 'qinghai', 'ningxia', 'xinjiang', 'beijing', 'tianjin', 'chongqing', 'xianggang', 'aomen'];
 
 var provincesText = ['上海', '河北', '山西', '内蒙古', '辽宁', '吉林', '黑龙江', '江苏', '浙江', '安徽', '福建', '江西', '山东', '河南', '湖北', '湖南', '广东', '广西', '海南', '四川', '贵州', '云南', '西藏', '陕西', '甘肃', '青海', '宁夏', '新疆', '北京', '天津', '重庆', '香港', '澳门'];
@@ -3850,17 +3848,27 @@ var proPieces = [
     {min: 1, max: 9, label: '确诊1-9人', color: '#f5bba7'},
 ];
 
-oBack.onclick = function () {
+/*
+* 返回全国按钮注册点击事件
+* */
+document.getElementById("whole").onclick = function () {
     initEcharts("china", "中国");
 };
 
 initEcharts("china", "中国");
 
-// 初始化echarts
+/*
+* 初始化echarts
+* */
 function initEcharts(pName, Chinese_) {
+
     console.log("pName:" + pName);
     var tmpSeriesData = [];
     if (pName === 'china') {
+        //隐藏省份疫情数据
+        var dom = document.getElementById("province-message");
+        dom.style.display="none";
+
         data.forEach((item)=>{
             //执行代码
             var ser = {
@@ -3948,6 +3956,8 @@ function initEcharts(pName, Chinese_) {
 
     if (pName === "china") { // 全国时，添加click 进入省级
         myChart.on('click', function (param) {
+            //在本页面跳转
+            window.location.hash='#centre';
             console.log(param.name);
             //遍历取到provincesText 中的下标  去拿到对应的省js
             for (var i = 0; i < provincesText.length; i++) {
@@ -3965,15 +3975,22 @@ function initEcharts(pName, Chinese_) {
     }
 }
 
-// 展示对应的省
+/*
+* 展示对应的省
+* */
 function showProvince(pName, Chinese_) {
+    //显示被隐藏的疫情数据
+    var dom = document.getElementById("province-message");
+    dom.style.display="block";
     //这写省份的js都是通过在线构建工具生成的，保存在本地，需要时加载使用即可，最好不要一开始全部直接引入。
     loadBdScript('$' + Chinese_ + 'JS', './js/map/province/' + Chinese_ + '.js', function () {
         initEcharts(pName, Chinese_);
     });
 }
 
-// 加载对应的JS
+/*
+* 加载对应的JS
+* */
 function loadBdScript(scriptId, url, callback) {
     initNewDefiniteLine();
     var script = document.createElement("script");
@@ -3995,20 +4012,78 @@ function loadBdScript(scriptId, url, callback) {
     document.getElementsByTagName("head")[0].appendChild(script);
 };
 
+//---------------------------------------------------------------------------------//
+
+$('#datetimepicker1').datetimepicker({
+    language:  'zh-CN',
+    todayBtn: 1,
+    autoclose: 1,
+    forceParse: 0,
+    minView: "month",
+    todayHighlight: 1,
+    format: 'yyyy-mm-dd',
+    startDate: '2019-11-01'
+});
+
+$("#datetimepicker1").datetimepicker("setDate", new Date() );  //设置显示默认当天的时间
+
+
+//------------------------------------------------------------------------------//
+
+/*
+* 地图现有确诊按钮
+* */
+document.getElementById("definite-bt").onclick=function () {
+    initEcharts("china", "中国");
+};
+
+/*
+* 地图累计确诊按钮
+* */
+document.getElementById("sum-definite-bt").onclick=function () {
+    initEcharts("china", "中国");
+};
+
+//---------------------------------------------------------------------------------------------//
+
+/*
+* 曲线图新增确诊按钮
+* */
+document.getElementById("echart-definite-bt").onclick=function () {
+    initNewDefiniteLine();
+};
+
+/*
+* 曲线图累计确诊按钮
+* */
+document.getElementById("echart-sum-definite-bt").onclick=function () {
+    initSumDefiniteLine();
+};
+
+/*
+* 曲线图治愈/死亡按钮
+* */
+document.getElementById("echart-cure-dead-bt").onclick=function () {
+    initCureAndDeadLine();
+};
+
+//-----------------------------------------------------------------------------------------------//
+
 /*
 * 新增确诊曲线
 * */
 function initNewDefiniteLine() {
     //获取给折线图准备的容器id
     var dom = document.getElementById("chart-line");
+    dom.setAttribute('class','active');
     var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
-    app.title = '新增确诊';
+    myChart.clear();
 
-    var colors = ['#5793f3', '#d14a61', '#675bba'];
-
-    option = {
+    var option = {
+        title: {
+            text: '新增确诊',
+            left: 'center'
+        },
         xAxis: {
             type: 'category',
             data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -4033,14 +4108,15 @@ function initNewDefiniteLine() {
 function initSumDefiniteLine() {
     //获取给折线图准备的容器id
     var dom = document.getElementById("chart-line");
+    dom.setAttribute('class','active');
     var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
-    app.title = '累计确诊';
+    myChart.clear();
 
-    var colors = ['#5793f3', '#d14a61', '#675bba'];
-
-    option = {
+    var option = {
+        title: {
+            text: '累计确诊',
+            left: 'center'
+        },
         xAxis: {
             type: 'category',
             data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
@@ -4054,6 +4130,7 @@ function initSumDefiniteLine() {
             smooth: true
         }]
     };
+
 
     myChart.setOption(option);
 }
@@ -4065,26 +4142,51 @@ function initCureAndDeadLine() {
     //获取给折线图准备的容器id
     var dom = document.getElementById("chart-line");
     var myChart = echarts.init(dom);
-    var app = {};
-    option = null;
-    app.title = '累计治愈/死亡';
+    myChart.clear();
 
-    var colors = ['#5793f3', '#d14a61', '#675bba'];
-
-    option = {
+    var option = {
+        title: {
+            text: '折线图堆叠'
+        },
+        tooltip: {
+            trigger: 'axis'
+        },
+        legend: {
+            data: ['邮件营销', '联盟广告']
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
         xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            boundaryGap: false,
+            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
         },
         yAxis: {
             type: 'value'
         },
-        series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line',
-            smooth: true
-        }]
+        series: [
+            {
+                name: '邮件营销',
+                type: 'line',
+                smooth: true,
+                stack: '总量',
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: '联盟广告',
+                type: 'line',
+                smooth: true,
+                stack: '总量',
+                data: [220, 182, 191, 234, 290, 330, 310]
+            }
+        ]
     };
 
     myChart.setOption(option);
 }
+
+//-----------------------------------------------------------------------------------------------//
