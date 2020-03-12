7bff4a3ea8c54f170ddecd8ca260cad2f66a1205
liliyuan <782821705@qq.com>
2020-03-12 19:28:42 +0800

    完善界面

--- front/js/map/index.js
+++ front/js/map/index.js
@@ -445,9 +445,29 @@ $('#datetimepicker2').datetimepicker({
     }
 });
 
+Date.prototype.format =function(format) {
+    var o = {
+        "M+" : this.getMonth()+1, //month
+        "d+" : this.getDate(), //day
+        "h+" : this.getHours(), //hour
+        "m+" : this.getMinutes(), //minute
+        "s+" : this.getSeconds(), //second
+        "q+" : Math.floor((this.getMonth()+3)/3), //quarter
+        "S" : this.getMilliseconds() //millisecond
+    }
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+        (this.getFullYear()+"").substr(4- RegExp.$1.length));
+    for(var k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,
+            RegExp.$1.length==1? o[k] :
+                ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
+}
+
 $("#datetimepicker1").datetimepicker("setDate", new Date());
 $("#datetimepicker2").datetimepicker("setDate", new Date());
-$("#date-input2").attr("value","2020-03-11");
+$("#date-input1").attr("value",new Date().format('yyyy-MM-dd'));
+$("#date-input2").attr("value",new Date().format('yyyy-MM-dd'));
 
 //------------------------------------------------------------------------------//
 
